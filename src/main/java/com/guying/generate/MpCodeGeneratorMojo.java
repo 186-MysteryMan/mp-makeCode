96f8ebd93f44a47c79a24b594d745da8265badf5
付鹏 <fupeng@puer.onaliyun.com>
2021-03-11 16:54:28 +0800

    init

---
+++ src/main/java/com/guying/generate/MpCodeGeneratorMojo.java
@@ -0,0 +1,59 @@
+package com.guying.generate;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.introspector.BeanAccess;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * @author guying
+ */
+@Mojo(name = "generator",defaultPhase = LifecyclePhase.COMPILE)
+public class MpCodeGeneratorMojo extends AbstractMojo {
+
+    private static final String DEFAULT_PATH = "mpGenerate.yaml";
+
+    private static final Logger logger = LoggerFactory.getLogger(AutoGenerator.class);
+
+    @Parameter(property = "configuration.file")
+    private String configurationFile;
+
+    public void execute() {
+        logger.info("------------------开始执行-----------------------");
+        InputStream inputStream = null;
+        if (!StringUtils.isEmpty(configurationFile)) {
+            try {
+                inputStream = new FileInputStream(configurationFile);
+            } catch (FileNotFoundException e) {
+                logger.error("读取配置文件异常,异常信息:{}",e.getMessage(),e);
+                e.printStackTrace();
+            }
+        } else {
+            inputStream = MpCodeGeneratorMojo.class.getClassLoader().getResourceAsStream(DEFAULT_PATH);
+        }
+        logger.info("-----------------开始读取配置文件-----------------------");
+        try{
+            Yaml yaml = new Yaml();
+            yaml.setBeanAccess(BeanAccess.FIELD);
+            AutoGenerator generator = yaml.loadAs(inputStream, AutoGenerator.class);
+            generator.execute();
+        }catch (Exception e){
+            logger.error("读取配置文件异常,异常信息:{}",e.getMessage(),e);
+        }
+    }
+}
---
+++ src/main/java/com/guying/generate/Test.java
@@ -0,0 +1,13 @@
+package com.guying.generate;
+
+/**
+ * @author guying
+ * @date 2021/03/10/18:39
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        MpCodeGeneratorMojo mpCodeGeneratorMojo = new MpCodeGeneratorMojo();
+        mpCodeGeneratorMojo.execute();
+    }
+}
